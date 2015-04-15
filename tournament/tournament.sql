bc2716feff9774a04ac652c4df6792a7b5902fc8
Mike Marchlik <mikeymarchlik@gmail.com>
2015-04-15 19:39:41 -0400

    got sql file in order

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -2,28 +2,15 @@
 create database tournament;
 
 --Add table for players
-create table players 
-(
-id serial PRIMARY KEY,
-name varchar 
-);
+
+
+create table players ( id serial PRIMARY KEY, name varchar );
 
 --Add table for each match containing winner and loser 
-create table match
-(
-id serial primary key,
-winner int  references player(id),
-loser int references player(id)
-);
+create table match ( id serial primary key, winner int  references players(id), loser int references players(id)); 
 
 --Add table for standings containing matches played and total score
-create table standings
-(
-id serial PRIMARY KEY,
-id int as players(id), 
-matches_played int,
-wins int
-);
+create table standings ( id serial PRIMARY KEY, id int references players(id), matches_played int, wins int);
 
 
 
